aa30aa651e8a4f70ab00264495ef30b0dff13add
Niklas Wagner <Skaro@Skaronator.com>
2016-10-08 02:59:06 +0200

    adjust default param

--- SQF/SKN-Escape.Tanoa/include/params.hpp
+++ SQF/SKN-Escape.Tanoa/include/params.hpp
@@ -19,21 +19,21 @@ class Params
             title = "Enemy Skill";
             values[] = {0, 1, 2, 3, 4};
             texts[] = {"Cadet", "Easy", "Normal", "Hard", "Extreme"};
-            default = 1;
+            default = 2;
 	};
 	class Param_EnemyFrequency
 	{
 		title="Enemy Groupsize";
 		values[]={1,2,3};
 		texts[]={"Few (suitable for 1-3 players)", "Some (suitable for 4-6 players)", "A lot (suitable for 7-8 players)"};
-		default = 1;
+		default = 2;
 	};
 	class Param_EnemySpawnDistance
 	{
 		title="Enemy Spawn Distance";
 		values[]={800,1050,1300};
 		texts[]={"Short (better performance)", "Medium", "Far (for good rigs)"};
-		default = 1300;
+		default = 1050;
 	};
 	class Param_VillageSpawnCount
 	{
@@ -61,7 +61,7 @@ class Params
 	    title="Time Of Day";
 		values[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
 		texts[]={"00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","Random"};
-		default = 24;
+		default = 5;
 		//function = "A3E_fnc_paramDaytime"; // (Optional) Function called when player joins, selected value is passed as an argument
 	};
 	class Param_TimeMultiplier	
@@ -69,7 +69,7 @@ class Params
 	    title="Time Multiplier (Fasttime)";
 		values[]={1,6,12,24,36};
 		texts[]={"1:1 (Normal)","1:6 (Day = 4 Hours)","1:12 (Day = 2 Hours)","1:24 (Day = 1 Hour)","1:36 (Day = 40 Minutes)"};
-		default = 1;
+		default = 12;
 	};
 	class Param_Weather {
 		title="Weather";
@@ -123,7 +123,7 @@ class Params
 		title="War-Torn mode (CSAT and AAF are fighting each other)";
 		values[]={0,1};
 		texts[]={"Disabled","Enabled"};
-		default = 0;
+		default = 1;
 	};
 	class Param_ReviveView
 	{
@@ -144,7 +144,7 @@ class Params
 		title="Additional Weaponbox (with less random content) at depots";
 		values[]={0,1};
 		texts[]={"Off", "On"};
-		default = 0;
+		default = 1;
 	};
 	class Param_NoNightvision
 	{
